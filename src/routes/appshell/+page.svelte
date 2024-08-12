2c64b3277cae25dfff008aff6d9a2c7084bed558
Patrick <Patrick@ShowYou.us>
2024-08-12 12:05:15 +0200

    Add /appshell route

---
+++ src/routes/appshell/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	// redirect to `/` if someone tries to access `/appshell`
+	// otherwise redirect to the current url so sveltekit renders the page
+	onMount(() =>
+		goto(location.pathname.startsWith('/appshell') ? '/' : location.pathname + location.search, {
+			replaceState: true
+		})
+	);
+</script>
