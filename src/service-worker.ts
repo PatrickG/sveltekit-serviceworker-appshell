cd32c9e2f79a81a3e319166321c3325a63c7a472
Patrick <Patrick@ShowYou.us>
2024-08-12 11:16:43 +0200

    Add types and declare `self` as `ServiceWorkerGlobalScope`

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,4 +1,9 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
+
 import { build, files, version } from '$service-worker';
 
 // Create a unique cache name for this deployment
