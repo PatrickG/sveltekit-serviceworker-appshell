3077f66f1aff9bbedb6d94bc63480bc2bdf6ad5d
Patrick <Patrick@ShowYou.us>
2025-10-03 12:46:02 +0200

    use `base`

--- src/routes/appshell/+page.svelte
+++ src/routes/appshell/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 
 	// redirect to `/` if someone tries to access `/appshell`
 	// otherwise redirect to the current url so sveltekit renders the page
 	onMount(() =>
-		goto(location.pathname.startsWith('/appshell') ? '/' : location.pathname + location.search, {
-			replaceState: true
-		})
+		goto(
+			location.pathname.startsWith(base + '/appshell') ? '/' : location.pathname + location.search,
+			{ replaceState: true }
+		)
 	);
 </script>
